887a230377635ce2faa7f8fabb9dd431980112fa
Anibal Linares <anibal.linares.19@gmail.com>
2019-04-05 21:56:13 -0300

    Se coloco comentarios para identificar que hacer la condicon

--- mayornumero.rb
+++ mayornumero.rb
@@ -6,9 +6,12 @@ puts "Ingresa Un Tercer Numero"
 num3 = gets.chomp.to_i
 
 if num1.to_i > num2.to_i and num2.to_i < num1.to_i and num3.to_i < num2.to_i
+  # Se evaluara que el numero 1 es mayor que todos
   puts " El Numero #{num1} es mayor"
 elsif num1.to_i < num2.to_i and num2.to_i < num3.to_i and num3.to_i > num2.to_i
+    # Se evaluara que el numero 3 es mayor que todos
   puts " El Numero #{num3} es mayor"
 elsif num1.to_i == num3.to_i and num2.to_i < num1.to_i and num3.to_i == num1.to_i
+    # Se evaluara que el numero 1 es igual que el numero 3 
   puts " El Numero #{num1} es igual #{num3}"
 end
