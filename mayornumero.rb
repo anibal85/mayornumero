1d75650147bee39f6a75924c76f9b1e862d1f1f5
Anibal Linares <anibal.linares.19@gmail.com>
2019-04-05 21:47:14 -0300

    Se Modifico el Mensajes de Salida

--- mayornumero.rb
+++ mayornumero.rb
@@ -1,11 +1,14 @@
+puts "Ingresa Un Numero"
 num1 = gets.chomp.to_i
+puts "Ingresa Un Segundo Numero"
 num2 = gets.chomp.to_i
+puts "Ingresa Un Tercer Numero"
 num3 = gets.chomp.to_i
 
 if num1.to_i > num2.to_i and num2.to_i < num1.to_i and num3.to_i < num2.to_i
-  puts "#{num1}"
+  puts " El Numero #{num1} es mayor"
 elsif num1.to_i < num2.to_i and num2.to_i < num3.to_i and num3.to_i > num2.to_i
-  puts "#{num3}"
+  puts " El Numero es #{num3} es menor"
 elsif num1.to_i == num3.to_i and num2.to_i < num1.to_i and num3.to_i == num1.to_i
-  puts "#{num3}"
+  puts " El Numero #{num1} es igual #{num3}"
 end
