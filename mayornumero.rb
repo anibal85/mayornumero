4fb739c9909afe5abfbe6829b16d61ecc58a21c5
Anibal Linares <48930195+anibal85@users.noreply.github.com>
2019-04-10 19:53:55 -0400

    Update mayornumero.rb
    Se realiza correcciones de la condicion

--- mayornumero.rb
+++ mayornumero.rb
@@ -5,13 +5,13 @@ num2 = gets.chomp.to_i
 puts "Ingresa Un Tercer Numero"
 num3 = gets.chomp.to_i
 
-if num1.to_i > num2.to_i and num1.to_i < num3.to_i
+if numero1.to_i >= numero2.to_i and numero1.to_i >= numero3.to_i
   # Se evaluara que el numero 1 es mayor que todos
   puts " El Numero #{num1} es mayor"
-elsif num2.to_i > num1.to_i and num2.to_i > num3.to_i and num3.to_i > num2.to_i
+elsif numero2.to_i >= numero1.to_i and numero2.to_i >= numero3.to_i
     # Se evaluara que el numero 3 es mayor que todos
   puts " El Numero #{num3} es mayor"
-elsif num1.to_i == num3.to_i and num2.to_i < num1.to_i and num3.to_i == num1.to_i
+elsif numero3.to_i >= numero1.to_i and numero3.to_i >= numero2.to_i
     # Se evaluara que el numero 1 es igual que el numero 3 
-  puts " El Numero #{num1} es igual #{num3}"
+  puts " El Numero #{num3} es el numero mayor"
 end
